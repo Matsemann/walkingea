8dfaf3a7ee262028e8b5c7cc2e74ed2f259c6598
Mats Krüger Svensson <mats@matsemann.com>
2017-04-20 19:30:45 +0200

    stuff

--- src/js/main.js
+++ src/js/main.js
@@ -2,9 +2,6 @@ import css from '../css/styles.css';
 
 import {World, Vec2, Edge, Box, DistanceJoint} from 'planck-js';
 
-console.log("whatevs");
-console.log("whatevs2");
-
 
 const world = new World(Vec2(0, -10));
 
@@ -16,7 +13,7 @@ const boxShape = Box(1, 1);
 
 const bodyDef = {
     type: 'dynamic',
-    position: Vec2(0, 5)
+    position: Vec2(0, 10)
 };
 
 const box = world.createBody(bodyDef);
@@ -26,16 +23,17 @@ box.createFixture(boxShape, 5);
 const jointDef = {
     frequencyHz: 2.0,
     dampingRatio: 0.2
+    //collideConnected: true
 };
 
 
 const distanceJoint = DistanceJoint(jointDef,
-    ground, Vec2(0, 10),
-    box, box.getWorldPoint(Vec2(0, 0))
+    ground, Vec2(-10, 7),
+    box, box.getWorldPoint(Vec2(.5, .5))
 );
 
 world.createJoint(distanceJoint);
-distanceJoint.setLength(7);
+distanceJoint.setLength(10);
 
 
 var canvas = document.getElementById('mainCanvas');
@@ -47,12 +45,10 @@ var camera = {
     zoom: 10,
 };
 
+
 setInterval(() => {
     world.step(1 / 60);
     var p = box.getPosition();
-    console.log(p);
-
-    var bodyList = world.getBodyList();
 
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.save();
@@ -63,23 +59,30 @@ setInterval(() => {
 
 
     drawRect(0, 0, 1, 1);
+    drawRect(-10, 10, .5, .5);
     drawRect(0, 30, 1, 1);
     drawRect(0, -30, 1, 1);
     drawRect(40, 0, 1, 1);
     drawRect(-40, 0, 1, 1);
 
-    drawRect(p.x, p.y, 5, 5);
-
-    var v = box.getFixtureList().getShape().m_vertices;
+    drawRect(p.x, p.y, 1, 1, box.getAngle());
 
     ctx.restore();
 
 }, 1000 / 30);
 
-function drawRect(x, y, w, h) {
-    ctx.fillRect(x - w / 2, y - h / 2, w, h);
+function drawRect(x, y, w, h, a) {
+    ctx.save();
+
+    ctx.translate(x + w/2, y + h / 2);
+    ctx.rotate(a);
+
+    //ctx.fillRect(x - w / 2, y - h / 2, w, h);
+    ctx.fillRect(- w / 2, - h / 2, w, h);
+
+    ctx.restore();
 }
 
-document.getElementById('zoom').addEventListener('change', () => {
+document.getElementById('zoom').addEventListener('input', () => {
     camera.zoom = document.getElementById('zoom').value;
 });
